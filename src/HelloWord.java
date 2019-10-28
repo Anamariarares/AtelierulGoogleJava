5838ae0e16bdaa42e5dbd60804c8e166abc7d589
Ana Moldovan <anamaria_rares@yahoo.com>
2019-10-28 20:30:28 +0200

    Hello Word

---
+++ src/HelloWord.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.sql.SQLOutput;
+import java.time.Clock;
+
+public class HelloWord {
+    public static void main(String args[]){
+        System.out.println("Hello word");
+        
+    }
+}
